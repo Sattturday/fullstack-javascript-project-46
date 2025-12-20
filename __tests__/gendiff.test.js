506a10648c16ddbb838bfcd3d1e5dea4a88ff26d
Sattturday <babara@flylady.su>
2025-12-20 19:09:04 +0300

    Add CI workflow and test configuration

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,19 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import genDiff from '../src/index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getFixturePath = name => path.join(__dirname, '..', '__fixtures__', name)
+const readFixtureFile = filename =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8')
+
+test('gendiff flat json', () => {
+  const file1 = getFixturePath('file1.json')
+  const file2 = getFixturePath('file2.json')
+  const expected = readFixtureFile('expected.txt').trim()
+
+  expect(genDiff(file1, file2)).toBe(expected.trim())
+})
